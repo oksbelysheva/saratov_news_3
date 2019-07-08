b81beb9043c33116732f35a377a54874199353fa
oksbelysheva <oks.belysheva@yandex.ru>
2019-07-08 09:54:09 +0400

    copy edit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
-const cityName = "Аткарск";
-const weatherData = {
+var cityName = "Аткарск";
+var weatherData = {
     key: "d9341ad1b5c8e3239b51c0d0abe05c01",
     city: "Atkarsk",
 }
@@ -8,7 +8,7 @@ const weatherData = {
 var d = new Date();
 var monthNames = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
 var date = document.getElementById("date");
-const city = document.getElementById('city');
+var city = document.getElementById('city');
 
 function getDate() {
     date.innerHTML = d.getDate() + " " + "<span class='dateMonth'>" + monthNames[d.getMonth()] + "<span class='dateMonth'>";
@@ -18,7 +18,20 @@ function getCity() {
     city.innerHTML = cityName;
 }
 
+function getTime() {
+    const d = new Date(),
+        hours = d.getHours(),
+        minutes = d.getMinutes(),
+        strTime = [hours % 24, (minutes < 10 ? "0" + minutes : minutes)].join(':');
+
+    time.innerHTML = strTime;
+    setTimeout(getTime, 1000);
+
+}
+
+
 getDate();
+getTime();
 getCity();
 
 const request_weather = "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -26,44 +39,44 @@ const request_weather = "https://api.openweathermap.org/data/2.5/weather?q=" +
     ",ru&units=imperial&appid=" +
     weatherData.key;
 const weatherTemp = document.getElementById('temp');
-//const weatherIcon = document.getElementById('icon');
+const weatherIcon = document.getElementsByClassName('header_weather_icon')[0];
 $.post(request_weather, function(data) {
     var f = data.main.temp;
     var c = Math.floor((f - 32.0) * 5.0 / 9.0);
     weatherTemp.innerHTML = (c > 0) ? "+" + c : c;
 
-    //const icon = "../img/weather/" + data.weather[0].main + ".png";
+    const icon = "../img/" + data.weather[0].main + ".png";
 
-    //weatherIcon.src = icon;
+    weatherIcon.src = icon;
 });
 
 $(document).ready(function() {
     $('.burger').on('click', function() {
         $(".burger").toggleClass('active');
-        $(".page-header__menu").toggleClass('page-header__menu-open');
+        $(".header__menu").toggleClass('header__menu-open');
+    });
+
+    $('.header__icons-search').on('click', function() {
+        $(".header-search-drop_down").toggleClass('active');
     });
 });
 
 
-var h_mrg = 0; // отступ когда шапка уже не видна  
-var h_hght = document.getElementById("page-header__top").offsetHeight;
-var advertisingTop = document.querySelector(".page-header-advertising img");
-
-$(function() {
-    var elem = $('.page-header__bottom');
+$(document).ready(function() {
+    var h_mrg = 0; // отступ когда шапка уже не видна  
+    var h_hght = document.getElementById("header__top").offsetHeight;
+    var elem = $('.header__bottom');
     var top = $(this).scrollTop();
 
     if (top == 0) {
-        advertisingTop.style.height = h_hght + "px";
         elem.css('top', h_hght);
     } else if (top > h_hght) {
         elem.css('top', h_mrg);
     }
 
     $(window).on("load resize scroll", function() {
-        h_hght = document.getElementById("page-header__top").offsetHeight;
+        h_hght = document.getElementById("header__top").offsetHeight;
         top = $(this).scrollTop();
-        advertisingTop.style.height = h_hght + "px";
         if (top + h_mrg < h_hght) {
             elem.css('top', (h_hght - top));
         } else {
